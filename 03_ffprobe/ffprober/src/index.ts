80bbaab99d6d5bc020ad6a2540c77a4cbb158572
Chris Guest <chris.guest@trint.com>
2021-12-05 17:04:27 +0000

    Output the json schema for the ffprobe data

--- 03_ffprobe/ffprober/src/index.ts
+++ 03_ffprobe/ffprober/src/index.ts
@@ -1,17 +1,59 @@
+import fs = require("fs");
+import md5File from 'md5-file';
+import path = require("path");
 import { logger } from "./logger";
 import Probe from "./probe";
 
+function getFilesizeInBytes(file: string) {
+    const stats = fs.statSync(file);
+    const fileSizeInBytes = stats.size;
+    return fileSizeInBytes;
+}
+
 async function main() 
 {
-    logger.info('Hello world!!!!')
-    let probe = new Probe();
-    const file = "/Volumes/videoshare//elephant-dreams-hls/stream_2/data183.ts"
-    let output = await probe.analyzeStreams(file)
-    let probedata = JSON.parse(output);
-    console.log(probedata)
+    const basePath = "/Volumes/videoshare"
+    const file = "elephant-dreams-hls/stream_2/data183.ts"
+    const outPath = "./out"
 
-    let gop = await probe.analyzeGOP(file)
-    console.log(gop)
+    let fullPath = path.join(basePath, file)
+
+    logger.info(`Analyse ${fullPath}`)
+
+    let probe = new Probe();
+    let output = await probe.analyzeStreams(fullPath)
+    let probedata = JSON.parse(output);
+    let gop = await probe.analyzeGOP(fullPath)
+
+    // merge gop and filename into video stream
+    let video = probedata.streams.filter((stream: any) => stream.codec_type == "video")
+    video[0]["gop"] = gop
+
+    // merge md5 of the file
+    const md5 = md5File.sync(fullPath)
+    const size = getFilesizeInBytes(fullPath)
+
+    let final = {
+        file: fullPath,
+        md5: md5,
+        size: size,
+        ...probedata
+    }
+
+    console.log(final)
+
+    // write to outpath
+    if (!fs.existsSync(outPath)) {
+        fs.mkdirSync(outPath);
+    }
+
+    let fullOutPath = path.join(outPath, "output.json")
+    fs.writeFile(fullOutPath, JSON.stringify(final, null, "\t"), function(err) {
+        if (err) {
+            logger.error(err);
+        } 
+        logger.info(`Created ${fullOutPath}`);
+    });
 }
 
 main()
--- 03_ffprobe/ffprober/src/probe.ts
+++ 03_ffprobe/ffprober/src/probe.ts
@@ -1,4 +1,5 @@
 import { spawnSync } from "child_process";
+import Papa from "papaparse";
 import { logger } from "./logger";
 
 //ffprobe -v quiet -print_format json=compact=1 -show_streams /Volumes/videoshare/sintel/sintel-x264-5.1.mp4 
@@ -10,16 +11,16 @@ export default class Probe {
     async run(file: string, options: Array<string>) {
         logger.info({ options: "ffprobe " + options.join(" ") });
         const spawnResult = spawnSync("ffprobe", options, {
-          cwd: process.cwd(),
-          env: process.env,
-          stdio: "pipe",
-          encoding: "utf-8",
+            cwd: process.cwd(),
+            env: process.env,
+            stdio: "pipe",
+            encoding: "utf-8",
         });
         //logger.info({"stdout":spawnResult.output});
         if (spawnResult.status !== 0) {
-          throw new Error(`ffprobe exited with ${spawnResult.status}`);
+            throw new Error(`ffprobe exited with ${spawnResult.status}`);
         } else {
-          return spawnResult.output;
+            return spawnResult.output;
         }
     }
 
@@ -30,27 +31,28 @@ export default class Probe {
     
         let realOut = "Error";
         if (out != null) {
-           realOut = out[1] || ""
+            realOut = out[1] || ""
         }
     
         return new Promise((resolve, reject) => {
-          resolve(realOut);
+            resolve(realOut);
         });
-      }    
+    }    
 
-      async analyzeGOP(file: string): Promise<string> {
+    async analyzeGOP(file: string): Promise<string> {
         let options = ["-select_streams", "v", "-show_frames", "-of", "csv", "-show_entries", "frame=key_frame,pict_type,best_effort_timestamp_time", file];
 
         let out = await this.run(file, options);
     
-        let realOut = "Error";
+        let frames = "Error"
         if (out != null) {
-           realOut = out[1] || ""
-        }
-    
+            let results = Papa.parse(out[1] || "", {});
+            frames = results.data.map((frame: any) => frame[3]).join("")
+        } 
+        
         return new Promise((resolve, reject) => {
-          resolve(realOut);
+            resolve(frames);
         });
-      }    
+    }    
 
 }
